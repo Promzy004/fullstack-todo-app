bf10bed4e4caef8c7884fafbc2136d87d9b2ef41
Promzy004 <promiseedwin242@gmail.com>
2025-09-20 18:25:01 +0100

    login handled

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,65 @@
+import { useState } from "react";
+import { useAuthStore } from "../store/AuthStore";
+import { BeatLoader } from "react-spinners";
+
+
+interface IFormData {
+  email: string
+  password: string
+}
+
+interface IFormErrors {
+    email?: string,
+    password?: string
+}
+
 const Login = () => {
+
+    const [ formData, setFormData ] = useState<IFormData>({
+        email: '',
+        password: '',
+    })
+    const [ formErrors, setFormErrors ] = useState<IFormErrors>({})
+    // const [ successMessage, setSuccessMessage ] = useState<string>('')
+    const login = useAuthStore(state => state.login)
+    const user = useAuthStore(state => state.user)
+    const loading = useAuthStore(state => state.loading)
+    console.log(loading)
+
+    const validate_form = () => {
+        const errors: IFormErrors = {}
+
+        //validate email
+        if(!formData.email.trim()) {
+            errors.email = 'Email is required'
+        } 
+
+        //validate password
+        if(!formData.password.trim()){
+            errors.password = 'Password is required'
+        } 
+
+        return errors
+    }
+
+    const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+
+        const validation_errors = validate_form()
+        setFormErrors(validation_errors)
+
+        if (Object.keys(validation_errors).length === 0) {
+            const resData = await login(formData.email, formData.password)
+            if (resData.success) {
+                console.log(resData)
+                console.log(user)
+            } else {
+                setFormErrors(resData?.errors?.errors)
+            }
+        }
+    }
+
+    
     return (
         <div className="flex items-center justify-center min-h-screen px-4">
             <div className="w-full max-w-md p-6 space-y-6 rounded-lg shadow-lg sm:p-8 bg-light dark:bg-dark">
@@ -13,7 +74,7 @@ const Login = () => {
                 {/* Form */}
                 <form className="space-y-4">
                     {/* Email */}
-                    <div>
+                    <div className="flex flex-col">
                         <label
                             htmlFor="email"
                             className="block mb-1 text-sm"
@@ -23,16 +84,16 @@ const Login = () => {
                         <input
                             type="email"
                             id="email"
-                            className="w-full px-3 py-2 border rounded-md bg-white
-                                        placeholder-gray-400 border-gray-300 
-                                        dark:bg-gray-700 dark:placeholder-gray-500 
-                                        focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            value={formData.email}
+                            onChange={(e) => setFormData({...formData, email:e.target.value})}
+                            className={`w-full px-3 py-2 border rounded-md bg-white placeholder-gray-400 dark:bg-gray-700 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 ${formErrors.email ? "border-red-400" : "border-gray-300"}`}
                             placeholder="Enter your email"
                         />
+                        {formErrors.email && <span className="text-[10px] text-red-500">{formErrors.email}</span>}
                     </div>
 
                     {/* Password */}
-                    <div>
+                    <div className="flex flex-col">
                         <label
                             htmlFor="password"
                             className="block mb-1 text-sm"
@@ -42,20 +103,25 @@ const Login = () => {
                         <input
                             type="password"
                             id="password"
-                            className="w-full px-3 py-2 border rounded-md bg-white
-                                        placeholder-gray-400 border-gray-300 
-                                        dark:bg-gray-700 dark:placeholder-gray-500 
-                                        focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            value={formData.password}
+                            onChange={(e) => setFormData({...formData, password:e.target.value})}
+                            className={`w-full px-3 py-2 border rounded-md bg-white placeholder-gray-400 dark:bg-gray-700 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 ${formErrors.password ? "border-red-400" : "border-gray-300"}`}
                             placeholder="Enter your password"
                         />
+                        {formErrors.password && <span className="text-[10px] text-red-500">{formErrors.password}</span>}
                     </div>
 
                     {/* Submit Button */}
                     <button
                         type="submit"
                         className="w-full py-2 font-semibold text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        onClick={handleSubmit}
                     >
-                        Sign In
+                        {loading ? 
+                            <BeatLoader color="#fff" loading={loading} size={10} />
+                            :
+                            <span>Sign In</span>  
+                        }
                     </button>
                 </form>
 
