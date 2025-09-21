e26e3adeab7b1acf072d7f46b6b05915870ac5bc
Promzy004 <promiseedwin242@gmail.com>
2025-09-21 17:12:48 +0100

    updated cors middleware to include some request method

--- backend/internal/middlewares/CORSMiddleware.go
+++ backend/internal/middlewares/CORSMiddleware.go
@@ -10,7 +10,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
         if origin == "http://localhost:5173" || origin == "http://127.0.0.1:5173" {
             w.Header().Set("Access-Control-Allow-Origin", origin)
         }
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT, OPTIONS")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
         w.Header().Set("Access-Control-Allow-Credentials", "true")
 
